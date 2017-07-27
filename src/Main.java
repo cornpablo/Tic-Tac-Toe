2d5f98d74450e76a7c738559c137c3796f5fb0b9
bigb0rd <terrorghosts@gmail.com>
2017-07-27 14:19:00 -0400

    Added a lot to Main
    Now I am working on getting the board to actually show on the screen. I have some of the basics, but I need to change make Spot inherit JButton so I can add the board to the layout.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
 
+public class Main extends JFrame {
+
+    public Main(String title) {
+        super(title);
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3,3));
+
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Main f = new Main("Tic Tac Toe");
+
+        Player player_1 = new Player("AJ","X");
+        Player player_2 = new Player("Sophie", "O");
+        Board board = new Board();
     }
 }
