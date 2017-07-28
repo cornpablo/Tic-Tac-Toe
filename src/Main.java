d7fa60fb247af60c88084da7e935c16a508f5adb
bigb0rd <terrorghosts@gmail.com>
2017-07-27 22:22:31 -0400

    Added and Changed Stuff for Turns
    Changed:
    -Window Size
    Added:
    -Formatting methods
    -Update method
    -Another row

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,34 +9,55 @@ public class Main extends JFrame{
     private static Player player_2 = new Player("Sophie", "O");
     private static Board board = new Board(player_1);
 
+    private static JLabel playerOneLabel = new JLabel(formatLabel(player_1));
+    private static JLabel playerTwoLabel = new JLabel(formatLabel(player_2));
+    private static JLabel currentPlayerLabel = new JLabel(formatCurrentPlayerLabel(board.getPlayerTurn()));
+
+    private static String formatLabel(Player player) {
+        return  player.getName() + " (" + player.getSymbol() + "): " + Integer.toString(player.getScore());
+    }
+
+    //I don't need to even get inputs honestly might remove it later on.
+    private static String formatCurrentPlayerLabel(Player player) {
+        return  "Current Player's Turn: " + player.getName();
+    }
+
     public Main(String title, Board board) {
         super(title);
 
-        setSize(400, 400);
+        setSize(600, 400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3,3));
+        panel.setLayout(new GridLayout(4,3));
         for (int i = 0; i < 9; ++i) {
             panel.add(board.getBoard().get(i));
             board.getBoard().get(i).addActionListener(new spotClicked());
         }
 
+        panel.add(currentPlayerLabel);
+        panel.add(playerOneLabel);
+        panel.add(playerTwoLabel);
+
         add(panel);
         setVisible(true);
+    }
+
+    void update() {
+        currentPlayerLabel.setText(formatCurrentPlayerLabel(board.getPlayerTurn()));
+        playerOneLabel.setText(formatLabel(player_1));
+        playerTwoLabel.setText(formatLabel(player_2));
     }
 
     public static void main(String[] args) {
 
         Main f = new Main("Tic Tac Toe", board);
 
-
     }
 
 
     public class spotClicked implements ActionListener {
-
         public void actionPerformed(ActionEvent e) {
             Object source = e.getSource();
             if (source instanceof Spot) {
@@ -45,11 +65,14 @@ public class Main extends JFrame{
                 if (spot.getAvailable()) {
                     spot.mark(board.getPlayerTurn());
 
+                    //Switches Players
                     if (player_1 == board.getPlayerTurn()) {
                         board.changeTurn(player_2);
                     } else {
                         board.changeTurn(player_1);
                     }
+
+                    update();
                 } else {
                     System.out.println("Spot has already been used!");
                 }
